e357ae02b276998f0c34c6f7d6618aea9406861b
relrekis <crazypide@gmail.com>
2026-07-19 20:20:18 +0300

    Add main.ts launcher and update gitignore for exe

---
+++ main.ts
@@ -0,0 +1,33 @@
+import "./server.ts";
+
+setTimeout(() => {
+  const edgePaths = [
+    "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+    "C:\\Program Files\\Microsoft\\Edge\\Application\\msedge.exe"
+  ];
+  let edgeExe = edgePaths.find(p => { try { Deno.statSync(p); return true; } catch { return false; } });
+  
+  if (!edgeExe) {
+    console.error("Edge not found!");
+    Deno.exit(1);
+  }
+
+  const userDataDir = Deno.env.get("LocalAppData") + "\\archivim-profile";
+  
+  const command = new Deno.Command(edgeExe, {
+    args: [
+      "--app=http://127.0.0.1:8384",
+      "--window-size=580,720",
+      "--user-data-dir=${userDataDir}",
+      "--disable-extensions",
+      "--disable-default-apps",
+      "--no-first-run",
+      "--disable-sync"
+    ]
+  });
+  
+  const process = command.spawn();
+  process.status.then(() => {
+    Deno.exit(0);
+  });
+}, 500);
